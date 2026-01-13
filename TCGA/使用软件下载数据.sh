16adc04ba143d15c59add9f7fff2a5fc3c56785c
Tao Zhang <156745192+taozhang22@users.noreply.github.com>
2026-01-13 14:02:47 +0800

    Update 使用软件下载数据.sh

--- TCGA/使用软件下载数据.sh
+++ TCGA/使用软件下载数据.sh
@@ -1,2 +1,2 @@
 cd /d "D:\research\bioinformatics\database\TCGA\COADREAD\expr_data"
-gdc-client.exe download -m "gdc_manifest.2026-01-09.213132.txt" --no-annotations
+gdc-client.exe download -m "gdc_manifest.2026-01-13.140006.txt" --no-annotations
